d9e4a8d2e7202fae688916bb2c8f67547bdbb74d
retardio73-boop <retardio73@gmail.com>
2026-09-14 02:49:16 -0300

    fix(transcript): export venue-time trust types

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ export {
 } from "./transcript.js";
 export type {
   TranscriptRecord, TranscriptRecordVerification, TranscriptStep, TranscriptFoldResult,
-  ContractHandshake,
+  TranscriptFoldOptions, VenueTimeTrust, ContractHandshake,
 } from "./transcript.js";
 
 export { lockTerms, MemoryRail } from "./rail.js";
